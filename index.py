d0b92278cef4b09a04a752e90e2c690fa87f38a5
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-07 21:28:53 -0500

    Update index.py

--- index.py
+++ index.py
@@ -2,3 +2,9 @@ import requests
 from flask import Flask, render_template_string
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Paso 1: Hacer la solicitud a la API
+    api_url = 'https://ejemplo.com/api/datos'
+    response = requests.get(api_url)
